b7cf8f9d69c1d547eb41db8fca81934dc3b46cca
Devin Dow <DevinDow@gmail.com>
2023-08-31 04:46:57 -0700

    Email, Password, Submit

--- lib/login_form.dart
+++ lib/login_form.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:email_validator/email_validator.dart';
 
 class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
   @override
   _LoginFormState createState() => _LoginFormState();
 }
@@ -18,9 +20,39 @@ class _LoginFormState extends State<LoginForm> {
       key: _formKey,
       child: Column(
         children: <Widget>[
-          // Email TextFormField
-          // Password TextFormField
-          // Submit Button
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Email'),
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) => setState(() => _email = value.trim()),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your email';
+              } else if (!EmailValidator.validate(value)) {
+                return 'Please enter a valid email';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(labelText: 'Password'),
+            obscureText: true,
+            onChanged: (value) => setState(() => _password = value.trim()),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                // Process login data
+                print('Email: $_email, Password: $_password');
+              }
+            },
+            child: const Text('Submit'),
+          ),
         ],
       ),
     );
