6a01cd2a0d9d2867ff3147560cf36815fefdac7d
Devin Dow <DevinDow@gmail.com>
2023-08-31 07:43:03 -0700

    manually added AutofillGroup, AutofillHints, & TextInput.finishAutofillContext()

--- lib/login_form.dart
+++ lib/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:email_validator/email_validator.dart';
 
@@ -17,44 +18,50 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Email'),
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (value) => setState(() => _email = value.trim()),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your email';
-              } else if (!EmailValidator.validate(value)) {
-                return 'Please enter a valid email';
-              }
-              return null;
-            },
+        key: _formKey,
+        child: AutofillGroup(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                autofillHints: const [AutofillHints.username],
+                onChanged: (value) => setState(() => _email = value.trim()),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  } else if (!EmailValidator.validate(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                autofillHints: const [AutofillHints.password],
+                onChanged: (value) => setState(() => _password = value.trim()),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    print('Email: $_email, Password: $_password');
+                    TextInput.finishAutofillContext();
+                    print('finished finishAutofillContext()');
+
+                    // Process login data
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
           ),
-          TextFormField(
-            decoration: const InputDecoration(labelText: 'Password'),
-            obscureText: true,
-            onChanged: (value) => setState(() => _password = value.trim()),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your password';
-              }
-              return null;
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                // Process login data
-                print('Email: $_email, Password: $_password');
-              }
-            },
-            child: const Text('Submit'),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
